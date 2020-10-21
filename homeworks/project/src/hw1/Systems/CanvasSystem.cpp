e3b0e261e0210f2a271cc3fe192beafee61a529b
Sucong <sucongjs@gmail.com>
2020-10-21 12:24:39 +0800

    画线时重复利用之前算好的点坐标

--- homeworks/project/src/hw1/Systems/CanvasSystem.cpp
+++ homeworks/project/src/hw1/Systems/CanvasSystem.cpp
@@ -1,14 +1,9 @@
 #include "CanvasSystem.h"
-
 #include "../Components/CanvasData.h"
-
-#include <math.h> 
-
+#include <math.h>
 #include <_deps/imgui/imgui.h>
-
 #include <eigen3/Eigen/Eigen>
-
-#include <atlstr.h>
+#include <atlstr.h>  // debug only
 
 using namespace Ubpa;
 
@@ -19,20 +14,9 @@ void CanvasSystem::OnUpdate(Ubpa::UECS::Schedule& schedule) {
 			return;
 
 		if (ImGui::Begin("Canvas")) {
-			ImGui::Checkbox("Enable grid", &data->opt_enable_grid);
-			ImGui::Checkbox("Enable context menu", &data->opt_enable_context_menu);
+			//ImGui::Checkbox("Enable grid", &data->opt_enable_grid);
+			//ImGui::Checkbox("Enable context menu", &data->opt_enable_context_menu);
 			ImGui::Checkbox("Draw Power Function", &data->func_power);
-
-			// Typically you would use a BeginChild()/EndChild() pair to benefit from a clipping region + own scrolling.
-			// Here we demonstrate that this can be replaced by simple offsetting + custom drawing + PushClipRect/PopClipRect() calls.
-			// To use a child window instead we could use, e.g:
-			//      ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));      // Disable padding
-			//      ImGui::PushStyleColor(ImGuiCol_ChildBg, IM_COL32(50, 50, 50, 255));  // Set a background color
-			//      ImGui::BeginChild("canvas", ImVec2(0.0f, 0.0f), true, ImGuiWindowFlags_NoMove);
-			//      ImGui::PopStyleColor();
-			//      ImGui::PopStyleVar();
-			//      [...]
-			//      ImGui::EndChild();
 
 			// Using InvisibleButton() as a convenience 1) it will advance the layout cursor and 2) allows us to use IsItemHovered()/IsItemActive()
 			ImVec2 canvas_p0 = ImGui::GetCursorScreenPos();      // ImDrawList API uses screen coordinates!
@@ -55,6 +39,8 @@ void CanvasSystem::OnUpdate(Ubpa::UECS::Schedule& schedule) {
 			const pointf2 mouse_pos_in_canvas(io.MousePos.x, io.MousePos.y);  // 运行窗口中Game中的坐标, 不是Canvas的坐标
 			const pointf2 point_pos(io.MousePos.x - origin.x, io.MousePos.y - origin.y);  // 点相对Canvas的坐标
 
+
+			static int points_size = -1;  // 通过点的数量来判断是否需要重新绘制线条(重新计算参数)
 			// 添加点
 			if (is_hovered && ImGui::IsMouseClicked(ImGuiMouseButton_Left))
 			{
@@ -63,9 +49,7 @@ void CanvasSystem::OnUpdate(Ubpa::UECS::Schedule& schedule) {
 
 			// Draw points 画点
 			for (int n = 0; n < data->points.size(); n++)
-			{
 				draw_list->AddCircle(data->points[n] + origin, 2.0f, IM_COL32(0, 255, 255, 200), 0, 3);
-			}
 
 			// Pan (we use a zero mouse threshold when there's no context menu)
 			// You may decide to make that threshold dynamic based on whether the mouse is hovering something etc.
@@ -99,30 +83,46 @@ void CanvasSystem::OnUpdate(Ubpa::UECS::Schedule& schedule) {
 			}
 			draw_list->PopClipRect();
 
+
 			// 多项式函数: 幂函数的线性组合
 			if (data->points.size() >= 2)  // 少于两个点, 插值无意义
 			{
 				if (data->func_power)
 				{
-					draw_func_power(draw_list, data->points, origin);
+					if (points_size != data->points.size())
+					{
+						points_size = data->points.size();
+						// need to recaculate the paramater 
+						draw_func_power(draw_list, data->points, origin, true);
+					}
+					else
+					{
+						draw_func_power(draw_list, data->points, origin, false);
+					}
 				}
 			}
 		}
-
 		ImGui::End();
 		});
 }
 
-void CanvasSystem::draw_func_power(ImDrawList* draw_list, std::vector<Ubpa::pointf2> v, ImVec2 origin)
+/*
+draw_list: 用来画线
+v: 用来取点的信息
+origin: 用来确定画布的位置
+needRecalculate: 是否需要重新计算参数a
+*/
+void CanvasSystem::draw_func_power(ImDrawList* draw_list, std::vector<Ubpa::pointf2> v, ImVec2 origin, bool needRecalculate)
 {
+
 	int size = v.size();
 	Eigen::MatrixXf X(size, size);
 	Eigen::VectorXf y(size);
-	Eigen::VectorXf a(size);  // 参数 X * a = y
-	float thickness = 3.0f;
+	Eigen::VectorXf a(size);  // 参数 X dot a = y
+	float thickness = 1.5f;
 	ImU32 color = IM_COL32(255, 0, 0, 200);
 
-	int min_x = v[0][0];  // 获得添加的点中最小的x值
+	int min_x = v[0][0];  // 获得添加的点中最小的x值, 坐标都是整数, 所以用int
 	int max_x = v[0][0];  // 获得添加的点中最大的x值
 	for (int i = 0; i < size; i++)
 	{
@@ -141,23 +141,38 @@ void CanvasSystem::draw_func_power(ImDrawList* draw_list, std::vector<Ubpa::poin
 
 	a = X.inverse() * y;
 
-	int density = 200;  // 插值的点数
-	int interval = 1 > (max_x - min_x) / density ? 1 : (max_x - min_x) / density;  // 插值点之间的间隔, 最小值是1
-	for (int x = min_x; x < max_x; x += interval)
+	///////////////////// 画线条 ////////////////////////////
+	//int density = 200;  // 插值的点数
+	//int interval = 1 > (max_x - min_x) / density ? 1 : (max_x - min_x) / density;  // 插值点之间的间隔, 最小值是1
+	int interval = 1;
+	static int y_tmp = -1;
+	for (int x1 = min_x-20; x1 < max_x+20; x1 += interval)  // 前后多画一些, 防止线接近垂直于x轴时没过点
 	{
-		Eigen::VectorXf x_v1(size);
+		int x2 = x1 + interval;
+		int y1;  // 由于是坐标点都是整型, 所以会有精度损失, 画出来的线条会锯齿
+		int y2;
+
+		// 计算y1
+		if(y_tmp == -1)  // 如果是画曲线的第一个小片段(曲线是由很多小线段拼起来的), y1要重新算
+		{
+			Eigen::VectorXf x_v1(size);
+			for (int i = 0; i < size; i++)
+				x_v1(i) = pow(x1, i);
+			y1 = a.dot(x_v1);
+		}
+		else  // y1可以直接用之前的
+			y1 = y_tmp;  // 可以直接用之前计算好的
+
+		// 计算y2
 		Eigen::VectorXf x_v2(size);
 		for (int i = 0; i < size; i++)
-		{
-			x_v1(i) = pow(x, i);
-			x_v2(i) = pow(x + interval, i);
-		}
+			x_v2(i) = pow(x2, i);
+		y2 = a.dot(x_v2);
 
-		//float y1 = (a * x_v1.transpose())(0);  // shape为1*1的一个值
-		//float y2 = (a * x_v2.transpose())(0);
-		int y1 = a.dot(x_v1);
-		int y2 = a.dot(x_v2);
-		draw_list->AddLine(ImVec2(x + origin.x, y1 + origin.y),
-			ImVec2(x + interval + origin.x, y2 + origin.y), color, thickness);
+		y_tmp = y2; // 保存计算好的y2值, 这是下一个线段的起点, 用来画下一个线段
+
+		draw_list->AddLine(ImVec2(x1 + origin.x, y1 + origin.y),
+						   ImVec2(x2 + origin.x, y2 + origin.y), color, thickness);
 	}
+	y_tmp = -1;  // 要复位一下, 下一波更新的时候线条的起点才正常
 }
